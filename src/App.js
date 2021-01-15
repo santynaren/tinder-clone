cfd4ba513ad8dfb07e73a7d8dad6cc9afedc579d
Narendra Santhosh Nagarajan <santhoshnarendra@gmail.com>
2021-01-15 12:22:10 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from './axios';
 import './App.css';
-
+import Header from 'components/Header';
 function App() {
+  const [array, setArray] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const req = await axios.get('http://localhost:8001/app/getCards');
+      console.log(req.data);
+      setArray(req.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi Hellgo</h1>
+      {/* header, tinder cards, swipe buttons */}
+      {array.map((val, idx) => {
+        return (
+          <div key={idx}>
+            <Header>{val.name}</Header>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css';
+const Header = ({children}) => {
+    return (
+        <div>
+            <h1>{children}</h1>
+        </div>
+    )
+}
+
+export default Header
