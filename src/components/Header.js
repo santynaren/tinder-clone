d7a3433671491b2c55a8468f74680bc93d4207e7
Narendra Santhosh Nagarajan <santhoshnarendra@gmail.com>
2021-01-15 18:04:01 +0530

    infsdf

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 import './Header.css';
-const Header = ({children}) => {
-    return (
-        <div>
-            <h1>{children}</h1>
-        </div>
-    )
-}
+const Header = ({ children }) => {
+  return (
+    <div>
+      <h1>{children}</h1>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
