3f1bd7bcc19abff3999848ec0deadd8ee66b8838
Narendra Santhosh Nagarajan <santhoshnarendra@gmail.com>
2021-01-15 12:22:30 +0530

    checking prettier

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import { useEffect, useState } from 'react';
+import {useEffect,useState} from 'react';
 import axios from './axios';
 import './App.css';
-import Header from 'components/Header';
+import Header from 'components/Header'
 function App() {
-  const [array, setArray] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const req = await axios.get('http://localhost:8001/app/getCards');
+  const [array,setArray] = useState([]);
+  useEffect(()=>{
+    const fetchData = async () =>{
+      const req = await axios.get("http://localhost:8001/app/getCards");
       console.log(req.data);
       setArray(req.data);
-    };
+    }
     fetchData();
-  }, []);
+  },[])
   return (
     <div className="App">
       <h1>Hi Hellgo</h1>
       {/* header, tinder cards, swipe buttons */}
-      {array.map((val, idx) => {
-        return (
-          <div key={idx}>
-            <Header>{val.name}</Header>
-          </div>
-        );
+      {array.map((val,idx)=>{
+        return(
+
+        
+<div key={idx}>
+<Header>{val.name}</Header>
+</div>
+        )
       })}
+   
     </div>
   );
 }
